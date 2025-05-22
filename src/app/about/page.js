7a73e171aa123b46fdd80176dc701e12568490cd
MrpZellen <158220937+MrpZellen@users.noreply.github.com>
2025-05-21 19:13:38 -0600

    small changes

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -7,6 +7,7 @@ export default function About() {
     const [loggedIn, setLoggedIn] = useState(false);
     return (
         <main className="flex min-h-screen flex-row p-10">
+        <title>About</title>
         <div className="relative flex h-[calc(100vh)] w-full max-w-[18rem] flex-col bg-transparent bg-clip-border p-4 text-blue-100 shadow-xl shadow-blue-gray-900/5">
     <div className="p-4 mb-2">
         <h5 className="block font-sans text-3xl antialiased font-semibold leading-snug tracking-normal text-blue-gray-900">
@@ -30,7 +31,7 @@ export default function About() {
         className="flex items-center w-full p-3 leading-tight transition-all rounded-lg outline-none text-start hover:bg-blue-gray-50 hover:bg-opacity-80 hover:text-lg focus:bg-blue-gray-50 focus:bg-opacity-80 focus:text-blue-gray-900 active:bg-blue-gray-50 active:bg-opacity-80 active:text-blue-gray-900">
         <div className="grid mr-4 place-items-center">
         </div>
-        My Photos Test
+        My Photos
         </div>
     </nav>
     </div>
